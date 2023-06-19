62ae699d7cba86157babf51f8087e0980a3c8513
Janne Ruponen <ruponez@gmail.com>
2023-06-19 12:07:25 +0300

    index created

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to My Website</title>
+</head>
+<body>
+    <h1>Welcome to My Website!</h1>
+    <p>This is the landing page of my website. Feel free to explore and learn more about what we have to offer.</p>
+
+    <nav>
+        <ul>
+            <li><a href="about.php">About</a></li>
+            <li><a href="services.php">Services</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
