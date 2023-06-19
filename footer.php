20f2b2ca97b600c6d3d63495ec2e55f7536a6c15
Janne Ruponen <ruponez@gmail.com>
2023-06-19 14:06:17 +0300

    +footer

---
+++ footer.php
@@ -0,0 +1,12 @@
+<!-- footer.php -->
+
+<footer>
+  <div class="container">
+    <ul>
+      <li><a href="#about">About</a></li>
+      <li><a href="#services">Services</a></li>
+      <li><a href="#contact">Contact</a></li>
+    </ul>
+    <p>&copy; 2023 SOFTA. All rights reserved.</p>
+  </div>
+</footer>
